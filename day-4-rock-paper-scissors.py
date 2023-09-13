e367ff2a7d61f81a5c98984b161c700ee9d381b7
viniscera <vinicius.silva.ep@outlook.com>
2023-09-13 00:16:44 -0300

    i got it, finally

--- day-4-rock-paper-scissors.py
+++ day-4-rock-paper-scissors.py
@@ -27,13 +27,29 @@ scissors = '''
 
 #Write your code below this line 👇
 
-answer = str(input('What do you choose? Type 0 for Rock, 1, for Paper or 2 for Scissors.'))
+import random
 
-if answer == '0':
-    print(rock)
-elif answer == '1':
-    print(paper)
-elif answer == '2':
-    print(scissors)
-else: 'Choose an option'    
-    
+list_answer = [rock, paper, scissors]
+
+# My answer
+
+my_answer = int(input('What do you choose? Type 0 for Rock, 1, for Paper or 2 for Scissors.\n'))
+my_result = list_answer[my_answer]
+
+# Computer answer
+
+computer_answer = random.randint(0,2)
+computer_result = list_answer[computer_answer]
+
+print(f"{my_result}\n Computer choose: {computer_result}")
+
+if my_answer == computer_answer:
+    print('Draw')
+elif my_answer == 0 and computer_answer == 1:
+    print('You lose')
+elif my_answer == 1 and computer_answer == 2:
+    print('You lose')
+elif my_answer == 2 and computer_answer == 0:
+    print('You lose')         
+else:
+    print('You won')

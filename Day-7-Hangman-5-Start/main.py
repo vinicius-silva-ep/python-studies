1c0440a53ce547100461fd70140cbf83e3e89ac6
viniscera <vinicius.silva.ep@outlook.com>
2023-10-16 22:39:21 -0300

    Day-7-Hangman-5-Start

--- Day-7-Hangman-5-Start/main.py
+++ Day-7-Hangman-5-Start/main.py
@@ -11,8 +11,9 @@ lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 
-from hangman_art import logo
+from hangman_art import logo, stages
 print(logo)
+
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -26,16 +27,19 @@ while not end_of_game:
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
 
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-
     #Check if user is wrong.
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -50,4 +54,5 @@ while not end_of_game:
         print("You win.")
 
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+
     print(stages[lives])

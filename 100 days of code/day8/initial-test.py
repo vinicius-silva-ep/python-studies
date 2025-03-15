9868e380fa01555f6c178c0827645737434d3483
vinicius-silva-ep <vinicius.silva.ep@outlook.com>
2025-03-15 11:55:16 +0000

    sorting the lessos again

---
+++ 100 days of code/day8/initial-test.py
@@ -0,0 +1,17 @@
+# Initial function
+
+def greet():
+    print("Hello!")
+    print("How are you doing?")
+    print("What's going on?")
+
+greet()    
+
+# Functions that allows for inputs
+
+def greet_with_name(name):
+    print(f"Hello! {name}")
+    print(f"How are you doing? {name}")
+    print(f"What's going on? {name}")
+
+greet_with_name("Vinicius")    

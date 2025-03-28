94ff90ab9d51528ce68932dc0f3f7ce65825e92d
vinicius-silva-ep <vinicius.silva.ep@outlook.com>
2025-03-28 21:07:03 +0000

    day-8-caesar-cipher

---
+++ 100 days of code/day8/caesar_cipher_1/main.py
@@ -0,0 +1,7 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# def encrypt(original_text, shift_amount):
